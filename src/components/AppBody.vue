a151d07dbe59d51e8e8c08e8b5bf8266f687c3b5
Georgiy Rzhavin <vimagik@gmail.com>
2024-11-20 14:13:42 +0300

    Added appbar component

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const data = ref(null)
 const error = ref(null)
@@ -8,14 +8,23 @@ fetch('https://fakestoreapi.com/products')
   .then((res) => res.json())
   .then((json) => (data.value = json))
   .catch((err) => (error.value = err))
+
+const searchStr = ref(null)
+
+const filteredData = computed(() => {
+  if (searchStr.value === null) return data.value
+  else {
+    return data.value.filter(product => product.title.indexOf(searchStr.value) > -1)
+  }
+})
 </script>
 
 <template>
   <v-container>
-    <h1 class="text-h2 text-center">Супер-пупер магазин</h1>
+    <AppHeader v-model="searchStr" />
     <v-progress-linear v-if="!data" class="mt-3" color="cyan" indeterminate></v-progress-linear>
     <v-row v-else class="d-flex justify-center mt-3">
-      <ProductCard class="mt-5" v-for="product in data" :key="product.id" :product-data="product" />
+      <ProductCard class="mt-5" v-for="product in filteredData" :key="product.id" :product-data="product" />
     </v-row>
   </v-container>
 </template>
