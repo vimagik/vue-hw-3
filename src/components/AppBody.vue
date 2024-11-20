4e42297998bf4d94527f5e282d6f74a2c067f6d4
Georgiy Rzhavin <vimagik@gmail.com>
2024-11-20 13:34:09 +0300

    first commit

---
+++ src/components/AppBody.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const data = ref(null)
+const error = ref(null)
+
+fetch('https://fakestoreapi.com/products')
+  .then((res) => res.json())
+  .then((json) => (data.value = json))
+  .catch((err) => (error.value = err))
+</script>
+
+<template>
+  <v-container>
+    <h1 class="text-h2 text-center">Супер-пупер магазин</h1>
+    <v-progress-linear v-if="!data" class="mt-3" color="cyan" indeterminate></v-progress-linear>
+    <v-row v-else class="d-flex justify-center mt-3">
+      <ProductCard class="mt-5" v-for="product in data" :key="product.id" :product-data="product" />
+    </v-row>
+  </v-container>
+</template>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-footer height="40" app>
+    Второе домашнее задание
+  </v-footer>
+</template>
